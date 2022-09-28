44ca6a76aae691a4350bb24e44adaab19811b9dc
tim <tim@example.com>
2022-09-29 00:12:33 +0800

    Remove writing check

--- client.c
+++ client.c
@@ -12,18 +12,12 @@ int main()
     long long sz;
 
     char buf[1];
-    char write_buf[] = "testing writing";
     int offset = 100; /* TODO: try test something bigger than the limit */
 
     int fd = open(FIB_DEV, O_RDWR);
     if (fd < 0) {
         perror("Failed to open character device");
         exit(1);
-    }
-
-    for (int i = 0; i <= offset; i++) {
-        sz = write(fd, write_buf, strlen(write_buf));
-        printf("Writing to " FIB_DEV ", returned the sequence %lld\n", sz);
     }
 
     for (int i = 0; i <= offset; i++) {
