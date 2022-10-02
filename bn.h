ada92753080a23aec8b3032b70cc109f5d4d7ae0
TIM <shinting840@gmail.com>
2022-10-02 16:32:12 +0800

    Initial commit

---
+++ bn.h
@@ -0,0 +1,64 @@
+#ifndef _BN_H_
+#define _BN_H_
+
+/*
+ * bignum data structure
+ * number[0] contains least significant bits
+ * number[size - 1] contains most significant bits
+ * sign = 1 for negative number
+ */
+typedef struct _bn {
+    unsigned int *number;
+    unsigned int size;
+    int sign;
+} bn;
+
+/*
+ * output bn to decimal string
+ * Note: the returned string should be freed with the kfree()
+ */
+char *bn_to_string(const bn *src);
+
+/*
+ * free entir bn data structur
+ * return 0 on success, -1 on error
+ */
+int bn_free(bn *src);
+
+/*
+ * copy the value from src to dest
+ * return 0 on success, -1 on error
+ */
+int bn_cpy(bn *dest, bn *src);
+
+/*
+ * compare length
+ * return 1 if |a| > |b|
+ * return -1 if |a| < |b|
+ * return 0 if |a| = |b|
+ */
+int bn_cmp(const bn *a, const bn *b);
+
+/*
+ * alloc a bn structure with the given size
+ * the value is initialized to +0
+ */
+bn *bn_alloc(size_t size);
+
+/* left bit shift on bn (maximun shift 31) */
+void bn_lshift(bn *src, size_t shift);
+
+/* c = a + b */
+void bn_add(const bn *a, const bn *b, bn *c);
+
+/* c = a - b */
+void bn_sub(const bn *a, const bn *b, bn *c);
+
+/* c = a x b */
+void bn_mult(const bn *a, const bn *b, bn *c);
+
+/* Calculate n-th Fibonacci number and save into dest */
+void bn_fib(bn *dest, unsigned int n);
+void bn_fib_fdoubling(bn *dest, unsigned int n);
+
+#endif /* _BN_H_ */
