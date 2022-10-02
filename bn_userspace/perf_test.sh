5faaeae22c6ad9f3bd0cb7a21723ab418588fba3
TIM <shinting840@gmail.com>
2022-10-02 18:51:00 +0800

    Fix shell script

--- bn_userspace/perf_test.sh
+++ bn_userspace/perf_test.sh
@@ -7,6 +7,6 @@ echo $VERSION
 perf stat -o stat_results_${VERSION}.txt -r 10 -e cycles,instructions,cache-misses,cache-references,branch-instructions,branch-misses ./fib
 
 perf record -g --call-graph dwarf ./fib
-perf report --stdio -g graph,0.5,caller >> >> report_results_${VERSION}.txt
+perf report --stdio -g graph,0.5,caller >> report_results_${VERSION}.txt
 
 rm *.data
