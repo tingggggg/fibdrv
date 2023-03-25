9f60d8e6bb54515aa8727057521a077a9a65e8ca
root <shinting840@gmail.com>
2023-03-25 11:08:41 +0800

    Add block structure to manage the node list

--- bn_userspace/mem_pool2/mem_pool.c
+++ bn_userspace/mem_pool2/mem_pool.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-typedef struct node {
-    int val;
-    struct node *next;
-} node_t;
-
 #define alloc_node(n, n_type, size)                                      \
     do {                                                                 \
         n = malloc(sizeof(n_type) * size);                               \
@@ -18,11 +12,33 @@ typedef struct node {
         }                                                                \
     } while (0);
 
+typedef struct node {
+    int val;
+    struct node *next;
+} node_t;
 
-void show(node_t *n)
+
+#define BLOCK_INS_NODE(b, n)                      \
+    do {                                          \
+        block_t **indirect = &b;                  \
+        block_t *new_b = malloc(sizeof(block_t)); \
+        new_b->node_list_head = n;                \
+        new_b->next = NULL;                       \
+        while (*indirect) {                       \
+            indirect = &(*indirect)->next;        \
+        }                                         \
+        *indirect = new_b;                        \
+    } while (0);
+
+typedef struct block {
+    node_t *node_list_head;
+    node_t *next;
+} block_t;
+
+void show_node_list(node_t *n)
 {
     while (n) {
-        printf("%d, ", n->val);
+        printf("%d (addr: %p), ", n->val, n);
         n = n->next;
     }
     printf("\n");
@@ -30,11 +46,24 @@ void show(node_t *n)
 
 int main()
 {
-    node_t *n;
-    alloc_node(n, node_t, 4);
+    printf("Sizeof(node_t): %zu\n", sizeof(node_t));
 
-    show(n);
+    node_t *n, *n2;
 
+    alloc_node(n, node_t, 6);
+    show_node_list(n);
+
+    alloc_node(n2, node_t, 4);
+    show_node_list(n2);
+
+    block_t *b = NULL;
+    BLOCK_INS_NODE(b, n);
+    BLOCK_INS_NODE(b, n2);
+
+    while (b) {
+        show_node_list(b->node_list_head);
+        b = b->next;
+    }
 
     return 0;
 }
