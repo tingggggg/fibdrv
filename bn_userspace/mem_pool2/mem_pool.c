19ba03a9f1b473ad499e86eed11a93ed054e45da
tingggggg <shinting840@gmail.com>
2023-03-29 11:36:52 +0800

    Remove useless structure

--- bn_userspace/mem_pool2/mem_pool.c
+++ bn_userspace/mem_pool2/mem_pool.c
@@ -62,38 +62,6 @@ void *my_malloc(MemoryPool *p, mem_size_t size)
 }
 
 
-
-#define alloc_node(n, n_type, size)                                      \
-    do {                                                                 \
-        n = malloc(sizeof(n_type) * size);                               \
-        for (int i = 1; i < size; i++) {                                 \
-            ((n_type *) ((char *) n + sizeof(n_type) * (i - 1)))->next = \
-                (char *) n + sizeof(n_type) * i;                         \
-        }                                                                \
-    } while (0);
-
-#define BLOCK_INS_NODE(b, n)                      \
-    do {                                          \
-        block_t **indirect = &b;                  \
-        block_t *new_b = malloc(sizeof(block_t)); \
-        new_b->node_list_head = n;                \
-        new_b->next = NULL;                       \
-        while (*indirect) {                       \
-            indirect = &(*indirect)->next;        \
-        }                                         \
-        *indirect = new_b;                        \
-    } while (0);
-
-
-void show_node_list(node_t *n)
-{
-    while (n) {
-        printf("%d (addr: %p), ", n->val, n);
-        n = n->next;
-    }
-    printf("\n");
-}
-
 int main()
 {
     printf("*****\n");
